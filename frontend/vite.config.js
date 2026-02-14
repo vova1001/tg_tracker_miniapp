c2d612bf92343c8f4cd657b84ec28c127bec9753
vova <vovaoleshko05@mail.ru>
2026-02-14 22:11:12 +0300

    vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // важно для корректной работы на статическом хостинге
+  base: './', // для статики
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  },
+  server: {
+    port: 5173,
+    open: true
+  }
 });
